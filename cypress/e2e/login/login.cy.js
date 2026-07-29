19829d09c94182eb387a0464851d96f3f28dff9c
danielavescia <danivescia@gmail.com>
2026-07-29 13:04:30 -0300

    test: enhance login tests with additional scenarios and validations

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -1,18 +1,133 @@
-describe('login', () => {
+describe('Login', () => {
+
+    beforeEach(() => {
+         cy.visit('https://www.saucedemo.com/');
+    })
 
     it('login with valid user', () => {
-        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"]').type("standard_user");
+   
+        cy.get('[data-test="password"]').type("secret_sauce");
 
-        cy.log("filling out username")
-        cy.get('[data-test="username"]').type("standard_user")
+        cy.get('[data-test="login-button"]').click();
 
-        cy.log("filling out password")
-        cy.get('[data-test="password"]').type("secret_sauce")
+        cy.url().should('eq', "https://www.saucedemo.com/inventory.html");
+    });
 
-        cy.log('submiting login form')
-        cy.get('[data-test="login-button"]').click()
+     it('should not login with invalid username', () => {
+        cy.get('[data-test="username"]').type("user");
+   
+        cy.get('[data-test="password"]').type("secret_sauce");
 
-        cy.log('redirected to /inventory')
-        cy.url().should('eq', "https://www.saucedemo.com/inventory.html")
-    })
-})
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('contain', 'Username and password do not match any user');
+
+        cy.url().should('eq', "https://www.saucedemo.com/");
+    });
+
+    it('should not login with invalid password', () => {
+        cy.get('[data-test="username"]').type("standard_user");
+   
+        cy.get('[data-test="password"]').type("secret");
+
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('contain', 'Username and password do not match any user');
+
+        cy.url().should('eq', "https://www.saucedemo.com/");
+    });
+
+    it('should not login with invalid username and password', () => {
+        cy.get('[data-test="username"]').type("user");
+   
+        cy.get('[data-test="password"]').type("secret");
+
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('contain', 'Username and password do not match any user');
+
+        cy.url().should('eq', "https://www.saucedemo.com/");
+    });
+
+    it('should not login with empty username and password', () => {
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('contain', 'Username is required');
+
+        cy.url().should('eq', "https://www.saucedemo.com/");
+    });
+
+    it('should not login with empty username', () => {
+        cy.get('[data-test="password"]').type('secret_sauce');
+
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('contain', 'Username is required');
+
+        cy.url().should('eq', "https://www.saucedemo.com/");
+    });
+
+    it('should not login with empty password', () => {
+        cy.get('[data-test="username"]').type('standard_user');
+
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('contain', 'Password is required');
+
+        cy.url().should('eq', "https://www.saucedemo.com/");
+    });
+
+    it('should not login with uppercase credentials', () => {
+        cy.get('[data-test="username"]').type("STANDARD_USER");
+   
+        cy.get('[data-test="password"]').type("SECRET_SAUCE");
+
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('contain', 'Username and password do not match any user');
+
+        cy.url().should('eq', "https://www.saucedemo.com/");
+    });
+
+    it('should not login with uppercase credentials', () => {
+        cy.get('[data-test="username"]').type("STANDARD_USER");
+   
+        cy.get('[data-test="password"]').type("SECRET_SAUCE");
+
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('contain', 'Username and password do not match any user');
+
+        cy.url().should('eq', "https://www.saucedemo.com/");
+    });
+
+    it('should not login with empty spaces in credentials', () => {
+        cy.get('[data-test="username"]').type("STANDARD_USER ");
+   
+        cy.get('[data-test="password"]').type(" SECRET_SAUCE");
+
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('contain', 'Username and password do not match any user');
+
+        cy.url().should('eq', "https://www.saucedemo.com/");
+    });
+
+    it('should not login with locked out user', () => {
+        cy.get('[data-test="username"]').type("locked_out_user");
+   
+        cy.get('[data-test="password"]').type("secret_sauce");
+
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('contain', 'user has been locked out');
+
+        cy.url().should('eq', "https://www.saucedemo.com/");
+    });
+});
+
+
+
+   
+
