43746c6e2c6747841b607ad5e1611096759f42ec
danielavescia <danivescia@gmail.com>
2026-07-30 09:59:10 -0300

    refactor: login tests for improved structure

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -2,76 +2,91 @@ describe('Login', () => {
 
     beforeEach(() => {
          cy.visit('/');
-    })
-
-    it('should login with valid user', () => {
-        cy.login('standard_user', 'secret_sauce');
-        cy.location('pathname').should('include', "/inventory.html")
-
-        cy.getCookie('session-username').should('have.property','value','standard_user');
     });
 
-    it('should not login with invalid username', () => {
-        cy.login('user', 'secret_sauce');
-
-        cy.assertLoginError('Username and password do not match any user');
-    });
-
-    it('should not login with invalid password', () => {
-        cy.login('standard_user', 'secret');
-
-        cy.assertLoginError('Username and password do not match any user');
-    });
-
-    it('should not login with invalid username and password', () => {
-        cy.login('user', 'secret');
-
-        cy.assertLoginError('Username and password do not match any user');
-    });
-
-    it('should not login with empty username and password', () => {
-        cy.get('[data-test="login-button"]').click();
+    context('with valid user', () =>{
         
-        cy.assertLoginError('Username is required');
+        it('should login with valid user', () => {
+            cy.login('standard_user', 'secret_sauce');
+            cy.location('pathname').should('include', "/inventory.html")
+
+            cy.getCookie('session-username').should('have.property','value','standard_user');
+        });
     });
 
-    it('should not login with empty username', () => {
-        cy.get('[data-test="password"]').type('secret_sauce');
-
-        cy.get('[data-test="login-button"]').click();
-
-        cy.assertLoginError('Username is required');
-    });
-
-    it('should not login with empty password', () => {
-        cy.get('[data-test="username"]').type('standard_user');
-
-        cy.get('[data-test="login-button"]').click();
-
-        cy.assertLoginError('Password is required');
-    });
-
-    it('should not login with uppercase credentials', () => {
-        cy.login('STANDARD_USER', 'SECRET_SAUCE');
-
-        cy.assertLoginError('Username and password do not match any user');
-    });
-
-    it('should not login with empty spaces in credentials', () => {
-        cy.login('standard_user ', ' secret_sauce');
+    context('with invalid credentials', () => {
         
-        cy.assertLoginError('Username and password do not match any user');
+        it('should not login with invalid username', () => {
+            cy.login('user', 'secret_sauce');
+
+            cy.assertLoginError('Username and password do not match any user');
+        });
+
+        it('should not login with invalid password', () => {
+            cy.login('standard_user', 'secret');
+
+            cy.assertLoginError('Username and password do not match any user');
+        });
+
+        it('should not login with invalid username and password', () => {
+            cy.login('user', 'secret');
+
+            cy.assertLoginError('Username and password do not match any user');
+        });
+    });
+   
+    context('when required fields are missing', () => {
+        
+        it('should not login with empty username and password', () => {
+            cy.get('[data-test="login-button"]').click();
+            
+            cy.assertLoginError('Username is required');
+        });
+
+        it('should not login with empty username', () => {
+            cy.get('[data-test="password"]').type('secret_sauce');
+
+            cy.get('[data-test="login-button"]').click();
+
+            cy.assertLoginError('Username is required');
+        });
+
+        it('should not login with empty password', () => {
+            cy.get('[data-test="username"]').type('standard_user');
+
+            cy.get('[data-test="login-button"]').click();
+
+            cy.assertLoginError('Password is required');
+        });
+    });
+   
+    context('when input format is invalid', () => {
+
+        it('should not login with uppercase credentials', () => {
+            cy.login('STANDARD_USER', 'SECRET_SAUCE');
+
+            cy.assertLoginError('Username and password do not match any user');
+        });
+
+        it('should not login with empty spaces in credentials', () => {
+            cy.login('standard_user ', ' secret_sauce');
+            
+            cy.assertLoginError('Username and password do not match any user');
+        });
+
+        it('should not login with blank spaces in credentials', () => {
+            cy.login('  ', '  ');
+
+            cy.assertLoginError('Username and password do not match any user');
+        });
     });
 
-    it('should not login with locked out user', () => {
-        cy.login('locked_out_user', 'secret_sauce');
+    context('when user has restrictions', () => {
+        
+        it('should not login with locked out user', () => {
+            cy.login('locked_out_user', 'secret_sauce');
 
-        cy.assertLoginError('user has been locked out');
-    });
-
-     it('should not login with blank spaces in credentials', () => {
-        cy.login('  ', '  ');
-
-        cy.assertLoginError('Username and password do not match any user');
-    });
+            cy.assertLoginError('user has been locked out');
+        });
+    });  
 });
