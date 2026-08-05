ad07d98a2913cfbb7c10409de4a7458e1b089018
danielavescia <danivescia@gmail.com>
2026-08-05 16:16:55 -0300

    test: add initial cart test suite with product addition and removal scenarios

---
+++ cypress/e2e/cart/cart.cy.js
@@ -0,0 +1,34 @@
+describe('Cart', () => {
+
+    context('when products are added to cart', () => {
+        it('should display selected product in cart', () => {
+
+        })
+
+        it('should display multiple added products in cart', () => {
+
+        })
+    })
+
+    context('when removing products from cart', () => {
+        it('it should remove', () => {
+
+        })
+
+        it('should display empty cart when removing the last product', () => {
+
+        })
+    })
+
+     context('when cart is empty', () => {
+        it('it should display empty cart', () => {
+
+        })
+    })
+
+    context('when cart state changes', () => {
+        it('should keep products in cart after page refresh', () => {
+
+        })
+    })
+});
