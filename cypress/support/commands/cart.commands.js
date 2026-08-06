d224f519d528fdfd739366ee8564e6b505838f47
danielavescia <danivescia@gmail.com>
2026-08-06 10:24:57 -0300

    test: enhance cart functionality with product addition and removal tests

--- cypress/support/commands/cart.commands.js
+++ cypress/support/commands/cart.commands.js
@@ -3,8 +3,15 @@ import  { cartElements } from '../pages/cart.elements';
 Cypress.Commands.add('assertOnCartPage', () => {
     cy.url().should('contain', '/cart.html');
     cy.get(cartElements.pageTitle).should('have.text', 'Your Cart')
-})
+});
 
 Cypress.Commands.add('navigateToCheckout', () => {
     cy.get(cartElements.checkoutButton).click();
-})
+});
+
+Cypress.Commands.add('removeItemFromCart', (product) => {
+    cy.contains(cartElements.item, product.name)
+        .find('button')
+        .contains('Remove')
+        .click();
+});
--- cypress/support/pages/cart.elements.js
+++ cypress/support/pages/cart.elements.js
@@ -1,8 +1,10 @@
 export const cartElements ={
 
     pageTitle: '[data-test="title"]',
+    itemsList: '[data-test="cart-list"]',
     item: '[data-test="inventory-item"]',
-    itemTitle: '[data-test="item-4-title-link"]',
+    itemName: '[data-test="inventory-item-name"]',
+    itemDescription: '[data-test="inventory-item-desc"]',
     itemPrice:'[data-test="inventory-item-price"]',
     checkoutButton: '[data-test="checkout"]',
 }
