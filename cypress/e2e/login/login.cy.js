490103bcf2e4541b5ef594a86d48b6dc52575ea3
danielavescia <danivescia@gmail.com>
2026-08-10 11:28:33 -0300

    refactor: adjust code to satisfy ESLint rules

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -1,93 +1,95 @@
 describe('Login', () => {
+  const errors = {
+    invalidCredentials: 'Username and password do not match any user',
+    usernameRequired: 'Username is required',
+    passwordRequired: 'Password is required',
+    lockedOutUser: 'user has been locked out',
+  };
 
-    const errors = {
-        invalidCredentials: 'Username and password do not match any user',
-        usernameRequired: 'Username is required',
-        passwordRequired: 'Password is required',
-        lockedOutUser: 'user has been locked out'
-    };
+  let users;
 
-    let users;
+  before(() => {
+    cy.fixture('users').then((data) => {
+      users = data;
+    });
+  });
 
-    before((() => {
-        cy.fixture('users').then((data) => {
-            users = data;
-        })
-    }));
+  context('with valid user', () => {
+    it('should login with valid user', () => {
+      cy.login({ username: users.standard.username, password: users.standard.password });
+      cy.assertOnCatalogPage();
+      cy.getCookie('session-username').should('have.property', 'value', 'standard_user');
+    });
+  });
 
-    context('with valid user', () => {
-        it('should login with valid user', () => {
-            cy.login({username: users.standard.username, password: users.standard.password});
-            cy.assertOnCatalogPage();
-            cy.getCookie('session-username').should('have.property','value','standard_user');
-        });
+  context('with invalid credentials', () => {
+    it('should not login with invalid username', () => {
+      cy.login({ username: 'user', password: users.standard.password });
+
+      cy.assertLoginError(errors.invalidCredentials);
     });
 
-    context('with invalid credentials', () => {
-        it('should not login with invalid username', () => {
-            cy.login({username:'user', password: users.standard.password});
+    it('should not login with invalid password', () => {
+      cy.login({ username: users.standard.username, password: 'secret' });
 
-            cy.assertLoginError(errors.invalidCredentials);
-        });
-
-        it('should not login with invalid password', () => {
-            cy.login({username: users.standard.username, password: 'secret'});
-
-            cy.assertLoginError(errors.invalidCredentials);
-        });
-
-        it('should not login with invalid username and password', () => {
-            cy.login({username: 'user', password: 'secret'});
-
-            cy.assertLoginError(errors.invalidCredentials);
-        });
-    });
-   
-    context('when required fields are missing', () => {
-        it('should not login with empty username and password', () => {
-            cy.login();
-           
-            cy.assertLoginError(errors.usernameRequired);
-        });
-
-        it('should not login with empty username', () => {
-            cy.login({password: users.standard.password})
-
-            cy.assertLoginError(errors.usernameRequired);
-        });
-
-        it('should not login with empty password', () => {
-            cy.login({username: users.standard.username})
-            
-            cy.assertLoginError(errors.passwordRequired);
-        });
-    });
-   
-    context('when input format is invalid', () => {
-        it('should not login with uppercase credentials', () => {
-            cy.login({username: users.standard.username.toUpperCase(), password: users.standard.password.toUpperCase()});
-
-            cy.assertLoginError(errors.invalidCredentials);
-        });
-
-        it('should not login with empty spaces in credentials', () => {
-            cy.login({username: 'standard_user ', password: ' secret_sauce'});
-            
-            cy.assertLoginError(errors.invalidCredentials);
-        });
-
-        it('should not login with blank spaces in credentials', () => {
-            cy.login({username: '  ', password: '  '});
-
-            cy.assertLoginError(errors.invalidCredentials);
-        });
+      cy.assertLoginError(errors.invalidCredentials);
     });
 
-    context('when user has restrictions', () => {
-        it('should not login with locked out user', () => {
-            cy.login({username: users.lockedOut.username, password: users.lockedOut.password});
+    it('should not login with invalid username and password', () => {
+      cy.login({ username: 'user', password: 'secret' });
 
-            cy.assertLoginError(errors.lockedOutUser);
-        });
-    });  
+      cy.assertLoginError(errors.invalidCredentials);
+    });
+  });
+
+  context('when required fields are missing', () => {
+    it('should not login with empty username and password', () => {
+      cy.login();
+
+      cy.assertLoginError(errors.usernameRequired);
+    });
+
+    it('should not login with empty username', () => {
+      cy.login({ password: users.standard.password });
+
+      cy.assertLoginError(errors.usernameRequired);
+    });
+
+    it('should not login with empty password', () => {
+      cy.login({ username: users.standard.username });
+
+      cy.assertLoginError(errors.passwordRequired);
+    });
+  });
+
+  context('when input format is invalid', () => {
+    it('should not login with uppercase credentials', () => {
+      cy.login({
+        username: users.standard.username.toUpperCase(),
+        password: users.standard.password.toUpperCase(),
+      });
+
+      cy.assertLoginError(errors.invalidCredentials);
+    });
+
+    it('should not login with empty spaces in credentials', () => {
+      cy.login({ username: 'standard_user ', password: ' secret_sauce' });
+
+      cy.assertLoginError(errors.invalidCredentials);
+    });
+
+    it('should not login with blank spaces in credentials', () => {
+      cy.login({ username: '  ', password: '  ' });
+
+      cy.assertLoginError(errors.invalidCredentials);
+    });
+  });
+
+  context('when user has restrictions', () => {
+    it('should not login with locked out user', () => {
+      cy.login({ username: users.lockedOut.username, password: users.lockedOut.password });
+
+      cy.assertLoginError(errors.lockedOutUser);
+    });
+  });
 });
