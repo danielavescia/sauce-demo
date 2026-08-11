84eef75ee55e5e38a0c92b98bfddcb9be4d8e27c
danielavescia <danivescia@gmail.com>
2026-08-11 11:05:00 -0300

    chore: update Cypress configuration and add specific run scripts for tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,11 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
   },
 });
