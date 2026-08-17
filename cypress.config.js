64a6bb2e37415e85e3d1f6bc5745b4c6296b2df2
danielavescia <danivescia@gmail.com>
2026-08-17 16:10:02 -0300

    chore: configure cypress grep

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'cypress';
+import { plugin as cypressGrepPlugin } from '@cypress/grep/plugin';
 
 export default defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      cypressGrepPlugin(config);
+      return config;
+    },
     baseUrl: 'https://www.saucedemo.com',
     viewportWidth: 1280,
     viewportHeight: 720,
